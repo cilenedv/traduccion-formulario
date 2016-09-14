f42c3d57ba0b96e3765cbccaaa4e939018bd90fe
cilenedv <cilenedelacruz@gmail.com>
2016-09-14 13:59:15 -0500

    Subiendo archivos iniciales

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.getElementsByTagName('h2')[0].innerHTML='Por favor inicia sesión';
+document.getElementById('inputEmail').placeholder='Correo Electrónico';
+document.getElementById('inputPassword').placeholder='Contraseña';
+document.querySelector('span').innerHTML='Recordar datos';
+document.querySelector('button').innerHTML='Inicia Sesión';
